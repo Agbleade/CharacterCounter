d13fc1b67e5e4d29f3e117bffd754a8b2c4dc427
Agbleade <yaw2004nhyira@gmail.com>
2025-08-20 06:04:14 -0500

    Third commit and this includes updating the web app to include a word count functionality, but it works efficiently assuming there is no mistake and the user keeps typing forward to clear any space

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 let count=0;
 const textspace = document.getElementById('inputspace');
 const counter = document.getElementById('actualcount');
+const wcounter = document.getElementById('wactualcount')
+let spaceCount = 0;
 
 textspace.addEventListener('input', ()=> {
    
     count = textspace.value.length;
     counter.textContent = count;
+    let trimmed = textspace.value.trimStart();
+    /*console.log(trimmed);*/
+ 
     
     
+})
+
+textspace.addEventListener('keydown', (e)=> {
+    if (e.code === 'Space') {
+        spaceCount++;
+        wcounter.textContent = spaceCount;
+    }
 })
 
 
 window.onload = () => {
     textspace.value = "";
     count = 0;
+    spaceCount = 0;
     counter.value = 0;
+    wcounter.value = 0;
     
 }
